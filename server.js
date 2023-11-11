afc4da8f180dba9ba4ef9a140c62c222d4cdd07a
Matt Groff <matt@umbrage.com>
2023-11-11 13:14:11 -0500

    Switch to scraping ebay search page instead of the api, and now using GPT-4 Vision for image and GPT-4 Turbo for search query creation

--- server.js
+++ server.js
@@ -1,67 +1,153 @@
 const { promises: fs } = require('fs');
+import OpenAI from 'openai';
 
-const EBAY_ACCESS_TOKEN = process.env.EBAY_ACCESS_TOKEN; // Make sure to have this in your .env
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
 
-// Function to fetch eBay sales history data with an optional condition filter
-async function searchItemSalesHistory(searchQuery, condition = 'UNSPECIFIED') {
-    try {
-        const encodedQuery = encodeURIComponent(searchQuery);
-        let url = `https://api.ebay.com/buy/marketplace_insights/v1_beta/item_sales/search?q=${encodedQuery}&limit=10`;
+// Function to fetch eBay sold listings from the website
+async function searchItemSalesHistory(searchQuery) {
+    console.log('searchQuery', searchQuery);
+    const formattedSearchQuery = searchQuery.split(' ').join('+');
+    const url = `https://www.ebay.com/sch/i.html?_nkw=${formattedSearchQuery}&LH_Sold=1&LH_Complete=1`;
 
-        // Append condition filter if not 'UNSPECIFIED'
-        if (condition !== 'UNSPECIFIED') {
-            url += `&filter=conditions:${condition}`;
-        }
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`eBay page request failed: ${response.statusText}`);
+    }
 
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${EBAY_ACCESS_TOKEN}`,
-                'Content-Type': 'application/json',
-                'X-EBAY-C-MARKETPLACE-ID': 'EBAY_US', // Always using the US marketplace
+    // Accumulate all text in a variable as we read through the stream
+    let allText = '';
+
+    // Set up the HTMLRewriter to parse the fetched page
+    const rewriter = new HTMLRewriter()
+        .on('li', {
+            element(el) {},
+        })
+        .on('li *', {
+            // Capture text from all descendants of 'li'
+            text(textChunk) {
+                // Accumulate all text chunks
+                allText += textChunk.text;
             },
         });
 
-        if (!response.ok) {
-            throw new Error(`eBay search request failed: ${response.statusText}`);
-        }
+    // Read through the entire stream to let HTMLRewriter do its work
+    await rewriter.transform(response).text();
 
-        const data = await response.json();
-        return data.itemSales; // The actual eBay API response will be used here
-    } catch (err) {
-        console.error(err);
-        return []; // Return an empty array on error
-    }
+    // Split the accumulated text by the 'Sold' keyword and filter out any empty entries
+    let rawListings = allText.split(/Sold Item/).filter(text => text.trim() !== '');
+
+    // The split operation removes the 'Sold Item' delimiter, so we add it back to each item except the first entry if it's not a valid listing
+    let listings = rawListings.map((text, index) => (index === 0 ? text : `Sold Item${text}`).trim());
+
+    // Filter the array to only include listings that contain the 'Sold' keyword, filter out any eBay announcements, and limit the results to the first 10 listings
+    listings = listings
+        .filter(listing => listing.includes('Sold'))
+        .filter(listing => !listing.includes('eBayAnnouncementsCommunitySecurity'))
+        .filter(listing => !listing.includes('ItemsFilter'))
+        .slice(0, 10);
+
+    return listings; // Return the filtered listings array
 }
 
 // Mock function to call OpenAI GPT-4 Vision Preview (replace with actual implementation)
 async function getSearchQueryFromImage(imageUrl, additionalDetails) {
     // This function should call the OpenAI GPT-4 Vision API and process the image and additional details to generate a search string.
     // Implement the API call to OpenAI with the provided imageUrl and additionalDetails and return the search string.
-    const simulatedResponse = {
-        searchTerm: 'iphone 8', // Replace with actual search term derived from the image
-        condition: 'NEW', // Assume the condition is "NEW" for this example
-    };
 
-    return simulatedResponse; // Return both the search term and condition}
+    try {
+        const messages = [
+            {
+                role: 'system',
+                content: `You are an expert eBay Search Agent with a knack for finding the best search terms for items based on images.\n
+          You will be provided an image and any additional details the user may have provided about the item.\n
+          Additional Details (if any): ${additionalDetails}\n
+          Based on the image and additional details, you should return the best search term for the item.\n
+          Your answer should JUST be the search term in raw text with no explanation as it will be directly plugged into an API. \n
+`,
+            },
+            {
+                role: 'user',
+                content: [
+                    { type: 'text', text: 'Give me the best search term to find the main focus of this image on eBay.' },
+                    {
+                        type: 'image_url',
+                        image_url: {
+                            url: imageUrl,
+                        },
+                    },
+                ],
+            },
+        ];
+
+        const chatCompletion = await openai.chat.completions.create({
+            // response_format: { type: 'json_object' }, TODO: Uncomment this line when the API supports JSON output
+            messages,
+            model: 'gpt-4-vision-preview',
+        });
+
+        return chatCompletion.choices[0].message.content;
+    } catch (error) {
+        console.error(error);
+        return new Response('Error calling OpenAI', { status: 500 });
+    }
 }
 
 // Mock function to select the best listing and suggest a price (replace with actual implementation)
-async function getBestListingAndPrice(listings) {
+async function getBestListingAndPrice(listings, searchQuery, additionalDetails) {
     // This function should call the OpenAI GPT-4 Turbo API with the eBay listings and return the best listing and a suggested price.
     // Implement the API call to OpenAI with the provided listings array.
-    return { bestListing: listings?.length ? listings[0] : null, suggestedPrice: 'suggested price based on analysis' }; // Placeholder return
+    try {
+        const messages = [
+            {
+                role: 'system',
+                content: `
+You are an expert eBay Reseller Agent with a knack for appraisal based on recent comparable item sales.\n
+You will be provided recently sold eBay listings for the searchQuery: ${searchQuery}\n 
+The user may have provided additional details about the item which would be helpful in the appraisal.\n
+Additional Details (if any): ${additionalDetails}\n
+Based on the recently sold listings, you should return the appraised price at which the user should expect their item to sell for.\n
+The json output should contain the following fields:\n
+- logic_for_the_appraised_price: a brief description of how and why you came up with this price based off the listings \n
+- appraised_price: the appraised price of the item\n
+`,
+            },
+            {
+                role: 'user',
+                content: `## Sold Listing \n${listings.map(listing => listing).join('## Sold Listing \n')})}`,
+            },
+        ];
+
+        const chatCompletion = await openai.chat.completions.create({
+            response_format: { type: 'json_object' },
+            messages,
+            model: 'gpt-4-1106-preview',
+        });
+
+        const output = chatCompletion.choices[0].message.content;
+
+        const parsedOutput = JSON.parse(output);
+
+        return {
+            logic_for_the_appraised_price: parsedOutput.logic_for_the_appraised_price,
+            appraised_price: parsedOutput.appraised_price,
+        };
+    } catch (error) {
+        console.log('Error calling OpenAI');
+        console.error(error);
+    }
 }
 
 // Function to handle the /search endpoint
 async function handleSearchRequest(req) {
     const { imageUrl, additionalDetails } = await req.json();
 
-    const searchQuery = await getSearchQueryFromImage(imageUrl, additionalDetails);
+    const searchTerm = await getSearchQueryFromImage(imageUrl, additionalDetails);
 
-    const listings = await searchItemSalesHistory(searchQuery);
+    const listings = await searchItemSalesHistory(searchTerm);
 
-    const finalResult = await getBestListingAndPrice(listings);
+    const finalResult = await getBestListingAndPrice(listings, searchTerm, additionalDetails);
 
     return new Response(JSON.stringify({ ...finalResult }), {
         status: 200,
